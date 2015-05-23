668ed2a7a4bb18fc9e61d7816e30059f77450066
jerrysdesign <jerry7722@hotmail.com>
2015-05-23 12:39:20 +0800

    將路徑整理為變數 將task整理為變數

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,78 @@
-var gulp = require('gulp'), 
-	jade = require('gulp-jade') ,
-	less = require('gulp-less') ,
-	notify = require("gulp-notify") ,
-	bower = require('gulp-bower'),
-	webserver = require('gulp-webserver'),
-	connect = require('gulp-connect');
+var gulp      = require('gulp'), 
+		jade      = require('gulp-jade') ,
+		less      = require('gulp-less') ,
+		notify    = require("gulp-notify") ,
+		bower     = require('gulp-bower'),
+		webserver = require('gulp-webserver'),
+		connect   = require('gulp-connect');
 
+// Sources config
+var source = {
+	resource : './resources',
+	jadePath : './resources/jade',
+	lessPath : './resources/less',
+	bowerDir : './bower_components' 
+}
 
 var config = {
-	jadePath: './resources/jade',
-	lessPath: './resources/less',
-	bowerDir: './bower_components' 
+	templates: {
+		main: [source.jadePath + '/index.jade'],
+		file: [source.jadePath + '/**/*.jade'],
+		watch: [source.jadePath + '/**/*.jade']
+	},
+	styles: {
+		main: [source.lessPath + '/style.less'],
+		file: [source.lessPath + '/**/*.less'],
+		watch: [source.lessPath + '/**/*.less']
+	},
+	dest: {
+		templates: './public',
+		styles: './public/css'
+	},
+	server: './public'
+}
+
+var buide = {
+	templates: config.templates.file,
+	styles: config.styles.file
 }
 
 gulp.task('bower', function() { 
 	return bower()
-		.pipe(gulp.dest(config.bowerDir)) 
+		.pipe(gulp.dest(source.bowerDir)) 
 });
 
 gulp.task('icons', function() { 
-	return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
+	return gulp.src(source.bowerDir + '/fontawesome/fonts/**.*') 
 		.pipe(gulp.dest('./public/fonts')); 
 });
 
 gulp.task('templates', function() {
-  gulp.src(config.jadePath+'/*.jade')
-    .pipe(jade({
-    	pretty: true
-    }))
-    .pipe(gulp.dest('./public'))
+	gulp.src(config.templates.file)
+		.pipe(jade({
+			pretty: true
+		}))
+		.pipe(gulp.dest(config.dest.templates))
 });
 
 gulp.task('css', function() { 
-	return gulp.src(config.lessPath + '/style.less')
+	return gulp.src(config.styles.main)
 		.pipe(less({
 			paths: [
-				'./resources/less',
-				config.bowerDir + '/bootstrap/less',
-				config.bowerDir + '/fontawesome/less'
+				source.lessPath,
+				source.bowerDir + '/bootstrap/less',
+				source.bowerDir + '/fontawesome/less'
 			]
 		}) 
 			.on("error", notify.onError(function (error) {
 				 return "Error: " + error.message;
 			}))) 
-		.pipe(gulp.dest('./public/css'))
+		.pipe(gulp.dest(config.dest.styles))
 		.pipe(connect.reload()); 
 });
 
 gulp.task('webserver', function() {
-	gulp.src('./public')
+	gulp.src(config.server)
 		.pipe(webserver({
 				host: 'localhost',
 				port: '5000',
@@ -59,7 +83,17 @@ gulp.task('webserver', function() {
 
 // Rerun the task when a file changes
  gulp.task('watch', function() {
-	gulp.watch(config.lessPath + '/*.less', ['css']); 
+	gulp.watch(config.styles.watch, ['css']); 
+	gulp.watch(config.templates.watch, ['templates']); 
 });
 
-gulp.task('default', ['bower', 'icons', 'templates', 'css', 'webserver', 'watch']);
+var tasks = [
+	'bower',
+	'icons',
+	'templates',
+	'css',
+	'webserver',
+	'watch'
+];
+
+gulp.task('default', tasks);
